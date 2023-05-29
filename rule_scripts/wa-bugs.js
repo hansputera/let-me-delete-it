a6cf093993d64736aff3e4fb136d7b4c6980c904
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-05-29 09:41:15 +0000

    feat: return true to delete the current context
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- rule_scripts/wa-bugs.js
+++ rule_scripts/wa-bugs.js
@@ -11,6 +11,7 @@ async function settingsUrl(context)
     const reply = context.getReply();
     if (reply && regex.test(reply.text)) {
         await reply.delete();
+        return true; // delete current context because the message has the wa.me/settings replied message
     }
     // else if (regex.test(context.text)) {
     //     await context.delete();
